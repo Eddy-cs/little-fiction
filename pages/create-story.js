1730c07c2841b0058cf968afc263489ba57a60ba
Debby <102255380+Eddy-cs@users.noreply.github.com>
2022-06-16 14:14:47 -0400

    Added components

--- pages/create-story.js
+++ pages/create-story.js
@@ -2,11 +2,20 @@
 //Add new story to main page
 
 import { Fragment } from "react";
+import NavbarLayout from "../components/Navbar";
+import { useRef } from "react";
 
 function CreateStory() {
+  const topicRef = useRef();
+
   return (
     <Fragment>
-      <h1>Create story!!</h1>
+      <NavbarLayout />
+      <h1>Create a story!!</h1>
+      <h1>Topic</h1>
+      <input ref={topicRef} type="text" />
+      <h1>Theme</h1>
+      <input type="text" />
     </Fragment>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 //Main page displaying all stories
 import { Fragment } from "react";
-import Link from "next/link";
+import { useState } from "react";
+import NavbarLayout from "../components/Navbar";
+import StoryList from "../components/StoryList";
 
 function HomePage() {
+  const [stories, setStories] = useState({
+    id: 1,
+    story: "This is a dummy story 1",
+  });
+
   return (
     <Fragment>
-      <h1>Here are some stories</h1>
-      <Link href="/create-story">Create new story</Link>
+      <NavbarLayout />
+      <StoryList stories={stories} />
     </Fragment>
   );
 }
