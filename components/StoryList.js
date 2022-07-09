b103006875f7fcf34c9c1481cc988837114ec073
Debby <102255380+Eddy-cs@users.noreply.github.com>
2022-07-09 19:57:17 -0400

    Added MUI styling

--- components/StoryList.js
+++ components/StoryList.js
@@ -1,7 +1,13 @@
 import { Fragment } from "react";
+import { Card } from "@mui/material";
 
 function StoryList(props) {
-  const list = props.stories.map((e) => <h1 key={e.id}>{e.story}</h1>);
+  const list = props.stories.map((e) => (
+    <Card key={e.id}>
+      <h1>Title</h1>
+      {e.story}
+    </Card>
+  ));
   // const list = props.stories.map((e) => <h1>{e}</h1>);
   // console.log(list);
 
