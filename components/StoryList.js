9607ea5f6f4f00e3c825bcf90b13fe3644c3463e
Eddy-cs <102255380+Eddy-cs@users.noreply.github.com>
2022-08-10 16:24:17 -0400

    Added favorites page

--- components/StoryList.js
+++ components/StoryList.js
@@ -3,11 +3,28 @@ import { Card, CardContent, Typography } from "@mui/material";
 import styles from "./StoryList.module.css";
 
 function StoryList(props) {
+  function lineBrake(storyBr) {
+    let text = [];
+    storyBr.split("\n").forEach((element) => {
+      text.push(<div>{element}</div>);
+    });
+    console.log(text);
+    return text;
+  }
+
+  // console.log(props.stories[0].img);
   const list = props.stories.map((e) => (
     <Card key={e.id}>
       <CardContent>
         <Typography variant="h4">{e.title}</Typography>
-        <Typography variant="body1">{e.story}</Typography>
+      </CardContent>
+      {e.img != undefined ? (
+        <img className={styles.list__img} src={e.img} alt={e.alt}></img>
+      ) : null}
+      <CardContent>
+        <Typography className={styles.list__lines} variant="body1">
+          {lineBrake(e.story)}
+        </Typography>
       </CardContent>
     </Card>
   ));
@@ -16,7 +33,7 @@ function StoryList(props) {
     <Fragment>
       <div className={styles.list__container}>
         <Typography pl={2} variant="h4">
-          Latest stories:
+          {props.pageTitle}
         </Typography>
         <div className={styles.list__stories}>{list}</div>
       </div>
--- pages/all-stories.js
+++ pages/all-stories.js
@@ -1,28 +1,24 @@
 import { Fragment, useEffect, useState } from "react";
 import StoryList from "../components/StoryList";
 
-function Favorites() {
+function AllStories() {
   const [stories, setStories] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function getStories() {
-      const response = await fetch("data.json", {
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      });
+      const response = await fetch("/api/firebase-config");
       const data = await response.json();
       setStories(data);
       setIsLoading(true);
     }
     getStories();
   }, []);
+
   return (
     <Fragment>
       {isLoading === true ? (
-        <StoryList stories={stories} pageTitle={"Favorite Stories"} />
+        <StoryList stories={stories} pageTitle={"Latest stories"} />
       ) : (
         <div>Loading...</div>
       )}
@@ -30,4 +26,4 @@ function Favorites() {
   );
 }
 
-export default Favorites;
+export default AllStories;
