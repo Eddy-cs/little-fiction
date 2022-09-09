fe768c78d17dcf33b415835bcc8aa752029f43d5
Eddy-cs <102255380+Eddy-cs@users.noreply.github.com>
2022-09-08 21:41:36 -0400

    Modified document title

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
-import styles from "../styles/app.module.css";
-import "../styles/globals.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import "../styles/globals.css";
+import styles from "../styles/app.module.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import Navbar from "../components/Nav";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 const muiTheme = createTheme({
   palette: {
@@ -17,6 +18,9 @@ function MyApp({ Component, pageProps }) {
     <Fragment>
       <ThemeProvider theme={muiTheme}>
         <CssBaseline />
+        <Head>
+          <title>Little Fiction</title>
+        </Head>
         <Navbar />
         <div className={styles.layout__grid}>
           <Component {...pageProps} />
