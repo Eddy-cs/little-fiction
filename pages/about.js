13c48d789557f5c856b1a88396d6f351cc8d0b57
Eddy-cs <102255380+Eddy-cs@users.noreply.github.com>
2022-11-17 21:14:27 -0500

    Added background image to about page

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,38 @@
 import { Typography } from "@mui/material";
 import styles from "../styles/about.module.css";
+import Image from "next/image";
+import { Fragment } from "react";
 
 export default function About() {
   return (
-    <div className={styles.about}>
-      <Typography variant="h4">
-        Little fiction is a tool that sparks creativity.
-      </Typography>
-      <Typography>
-        This app uses the GPT-3 AI model to demonstrate a simple but interesting
-        use case of this powerful technology. If you are more interested check
-        the openAI website for more information.
-      </Typography>
-      <Typography>
-        DALLE-2 pictures are added manually to the favorite stories while openAI
-        releases an official API.
-      </Typography>
-    </div>
+    <Fragment>
+      <div className={styles.about}>
+        <Typography variant="h4">
+          Little fiction is a tool that sparks creativity.
+        </Typography>
+        <Typography>
+          This app uses the GPT-3 AI model to demonstrate a simple but
+          interesting use case of this powerful technology. If you are more
+          interested check the openAI website for more information.
+        </Typography>
+        <Typography>
+          DALLE-2 pictures are added manually to the favorite stories while
+          openAI releases an official API.
+        </Typography>
+      </div>
+      <div
+        style={{
+          opacity: 0.1,
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+        }}
+      >
+        <Image width={500} height={500} src={"/composition.png"}></Image>
+      </div>
+    </Fragment>
   );
 }
